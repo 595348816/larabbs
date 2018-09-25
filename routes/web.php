5022573376a6e670034d38e5ac3b6d90fd398211
zxp_lethe <zxp_lethe@163.com>
2018-09-25 18:25:08 +0800

    上传图片

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::get('/','PagesController@root')->name('root');
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories','CategoriesController',['only'=>['show']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
