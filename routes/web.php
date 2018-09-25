b5f3ebee6895a7dbd1eb94e95357549a8fcc0f7c
zxp_lethe <zxp_lethe@163.com>
2018-09-25 16:37:04 +0800

    话题分类

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::get('/','PagesController@root')->name('root');
     Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories','CategoriesController',['only'=>['show']]);
