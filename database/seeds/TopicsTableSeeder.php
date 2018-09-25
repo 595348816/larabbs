277e1a8f61945ca81eeed3db02ea1b72f6ccbee0
zxp_lethe <zxp_lethe@163.com>
2018-09-25 16:03:15 +0800

    注册话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -7,10 +7,20 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        //所有用户id数组
+        $user_ids=\App\Models\User::all()->pluck('id')->toArray();
+        //所有分类id数组
+        $category_ids=\App\Models\Category::all()->pluck('id')->toArray();
+
+        $facker=app(\Faker\Generator::class);
+
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index) use($user_ids,$category_ids,$facker) {
+                $topic->user_id=$facker->randomElement($user_ids);
+                $topic->category_id=$facker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
