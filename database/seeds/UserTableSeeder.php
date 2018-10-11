07520e132cce82e59e98a2addf24b72a3d4bd24c
zxp_lethe <zxp_lethe@163.com>
2018-10-11 20:53:07 +0800

    权限

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -41,9 +41,12 @@ class UserTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
+        $user->assignRole('Founder');
         $user->name = 'lethe';
         $user->email = 'zxp_lethe@163.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'LaraBBS') - {{ setting('site_name','LaraBBS') }}</title>
     <meta name="description" content="@yield('description','LaraBBS 爱好者社区')">
 
     <!-- Styles -->
@@ -25,6 +26,9 @@
 
         @include('layouts._footer')
     </div>
+    @if (app()->isLocal())
+        @include('sudosu::user-selector')
+    @endif
 {{-- script--}}
     <script src="{{ asset('js/app.js')}}"></script>
     @yield('scripts')
